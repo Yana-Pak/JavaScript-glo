c76cc480736c634e14eb94a7dbd44928fc9c9218
Yana <qwq.yana@yandex.ru>
2024-09-22 12:28:51 +0300

    работа с примитивными переменными

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+let title = 'glo- project';
+let screens = 'Простые, Сложные, Интерактивные';
+let screenPrice = 7;
+let rollback = 25;
+let fullPrice = 5500000;
+let adaptive = true;
 
-alert('Сообщение');
-console.log('Вывод в консоль');
+// Вывести в консоль тип данных значений переменных title, fullPrice, adaptive;
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+
+// Вывести в консоль длину строки из переменной screens
+console.log(screens.length);
+
+
+// Вывести в консоль “Стоимость верстки экранов (screenPrice) рублей/ долларов/гривен/юани” и “Стоимость разработки сайта (fullPrice) рублей/ долларов/гривен/юани”
+console.log(`Стоимость верстки экранов ${screenPrice} рублей/ долларов/гривен/юани`);
+console.log(`Стоимость разработки сайта ${fullPrice} рублей/ долларов/гривен/юани`);
+
+// Привести строку screens к нижнему регистру и разбить строку на массив, вывести массив в консоль
+console.log(screens.toLowerCase());
+
+// Вывести в консоль Процент отката посреднику за работу (fullPrice * (rollback/100))
+let percentRollback = fullPrice * (rollback / 100);
+console.log(percentRollback);
+// или
+// console.log(fullPrice * (rollback / 100));
